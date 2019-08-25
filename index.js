b095cf9abb50aaab53700c8c821f6578e27811bc
Elena Hernandez <hrndzelena@gmail.com>
2019-08-25 12:32:42 -0500

    get, post, put & delete

--- index.js
+++ index.js
@@ -11,13 +11,29 @@ app.use(parser.json());
 
 app.get('/', async (req, res) => {
   const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
-  res.send(data);
+  res.status(200).send(data);
 });
 
-app.post('/', async(req, res) =>{
-  const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+app.post('/', async(req, res) => {
+  const { body } = req
+  const { data } = await axios.post('https://jsonplaceholder.typicode.com/users', body)
 
-  res.send(data);
+  res.status(201).send(data);
+});
+
+app.put('/:id', async(req, res) => {
+  const { body } = req
+  const { id } = req.params
+  await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, body)
+
+  res.sendStatus(204)
+});
+
+app.delete('/:id', async(req, res) => {
+  const { id } = req.params
+  const { data } = await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+
+  res.sendStatus(204);
 });
 
 app.listen(3000, function () {
